c49fd5fdd52ea6d94d22bc9dac2827f936ca3b75
suman-sundaram <sr03319@gmail.com>
2025-09-23 07:52:25 +0530

    Update ex4.js

--- ex4.js
+++ ex4.js
@@ -1,3 +1,14 @@
- 
+ function apple() {
+    console.log("hello apple")
+ }
+function samsung() {
+    console.log("hello samsung")
+}
+function vivo() {
+    console.log("hello vivo")
+}
+apple()
+samsung()
+vivo() 
 
 
